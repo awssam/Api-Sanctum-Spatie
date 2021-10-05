168886b9cd9ae5a0543b047720d1ef4d0ef2028e
Awssam <awtsoft@gmail.com>
2021-10-05 16:39:34 +0100

    cleaning tests

--- routes/api.php
+++ routes/api.php
@@ -8,60 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
 
+
+
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/me', function (Request $request) {
-        auth()->user()->getRoleNames();
-            auth()->user()->getDirectPermissions();
-            return auth()->user();
-            
-         if(auth()->user()->can('read')){
-            
-            auth()->user()->getRoleNames();
-            auth()->user()->getDirectPermissions();
-            return 'can read';
-        }
-        if(auth()->user()->can('ilias')){
-            
-
-            auth()->user()->getRoleNames();
-            auth()->user()->getDirectPermissions();
-            return auth()->user();
-        }
-        if(auth()->user()->can('create')){
-            
-            auth()->user()->getRoleNames();
-            auth()->user()->getDirectPermissions();
-            return auth()->user();
-        }
-    });
-
-   
-
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    
 
 
-
-    Route::get('/roles', function (Request $request) {
-        return  \Spatie\Permission\Models\Role::all();
-    });
-    Route::get('/permissions', function (Request $request) {
-        return  \Spatie\Permission\Models\Permission::all();
-    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -69,14 +23,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
 // When the route is not defined
 Route::get('/{any}', function () {
-    return response()->json([
-        'status' => 'Error',
-        'message' => "The route is not defind!"
-    ], 404);
-})->where('any', '.*')->name('notFound');
-
-
-Route::post('/{any}', function () {
     return response()->json([
         'status' => 'Error',
         'message' => "The route is not defind!"
