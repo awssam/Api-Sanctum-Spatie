eebc85e8fb6d70387b712e4d7209f5ce0dedf965
Awssam <awtsoft@gmail.com>
2021-10-05 15:56:29 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -42,28 +42,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
 
 
-        $users = [
-            [
-                'email'=>'awssam@awssam.com',
-                'name'=>'awssam',
-                'password'=>bcrypt('1993830')
-            ]
-        ];
-
-        foreach ($users as $user){
-            $u = User::updateOrCreate(['email' => $user['email']], $user);
-            $u->assignRole('super-admin');
-        }
-
-
-
-
-
-
-
-
-
-
 
 
 
